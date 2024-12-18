e5b7de05261fa67130beb9e3888f9cc52130bcb7
Subramanya <subramanya.adiga4@protonmail.com>
2024-12-18 20:15:11 +0530

    Added Color highlight Plugin

---
+++ nvim/lua/subramanya/lazy_plugins/colorhighlight.lua
@@ -0,0 +1,17 @@
+return {
+	"brenoprata10/nvim-highlight-colors",
+	opts = {
+		render = "background",
+		virtual_symbol = "■",
+		virtual_symbol_prefix = "",
+		virtual_symbole_sufix = " ",
+		virtual_symbol_position = "inline",
+		enable_hex = true,
+		enable_short_hex = true,
+		enable_rgb = true,
+		enable_hls = true,
+		enable_named_colors = true,
+		exclude_filetypes = {},
+		exclude_buftypes = {},
+	},
+}
