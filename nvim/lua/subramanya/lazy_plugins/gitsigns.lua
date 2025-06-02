f307e4c8bcaba54738df9a33fa5711fcea16d26b
Subramanya <subramanya.adiga4@protonmail.com>
2025-06-02 19:38:29 +0530

    Changed Gitsigns Gutter Signs

--- nvim/lua/subramanya/lazy_plugins/gitsigns.lua
+++ nvim/lua/subramanya/lazy_plugins/gitsigns.lua
@@ -1,6 +1,13 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "-" },
+			topdelete = { text = "‾" },
+			chanagedelete = { text = "~" },
+		},
 		on_attach = function(bufnr)
 			local gitsigns = require("gitsigns")
 
