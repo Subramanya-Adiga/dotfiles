a8cccdeb0b7e2f405d63ab20429c08c8e2390ca0
Subramanya <subramanya.adiga4@protonmail.com>
2025-06-02 20:28:45 +0530

    Removed Redundant Mini Statusline

--- nvim/lua/subramanya/lazy_plugins/mini.lua
+++ nvim/lua/subramanya/lazy_plugins/mini.lua
@@ -1,11 +1,5 @@
 return {
 	"echasnovski/mini.nvim",
 	version = false,
-	config = function()
-		local status_line = require("mini.statusline")
-		status_line.setup({ use_icons = vim.g.have_nerd_font })
-		status_line.section_location = function()
-			return "%2l:%-2v"
-		end
-	end,
+	-- opts = {},
 }
