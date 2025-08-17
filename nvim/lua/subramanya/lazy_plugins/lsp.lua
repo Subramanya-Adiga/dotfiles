49f8175b80311413a6eedb75eb333e6daa62637a
Subramanya <subramanya.adiga4@protonmail.com>
2025-08-17 13:06:26 +0530

    Changed Cmake to Cmakelang And Added GLSL Analyzer

--- nvim/lua/subramanya/lazy_plugins/lsp.lua
+++ nvim/lua/subramanya/lazy_plugins/lsp.lua
@@ -146,7 +146,8 @@ return {
 			ols = {},
 			zls = {},
 			rust_analyzer = {},
-			cmake = {},
+			cmakelang = {},
+			glsl_analyzer = {},
 			hyprls = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			lua_ls = {
